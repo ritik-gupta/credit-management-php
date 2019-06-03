07a9cb91d9165fb82caeac4701c52432d32047a9
Ritik Gupta <ritikg8126@gmail.com>
2019-06-03 17:56:57 +0530

    Modified homepage

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+    require_once('db_connect.php');
+
+    $query = "select * from users";
+    $result = mysqli_query($link,$query);
+
+?>
+
+<html>
+	<head>
+        <meta charset="utf-8">
+        <title>Credit Management</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css" integrity="sha384-MIwDKRSSImVFAZCVLtU0LMDdON6KVCrZHyVQQj6e8wIEJkW4tvwqXrbMIya1vriY" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/js/bootstrap.min.js" integrity="sha384-ux8v3A6CPtOTqOzMKiuo3d/DomGaaClxFYdCu2HPMBEkf6x2xiDyJ7gkXU0MWwaD" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="css/style.css">
+        
+    </head>
+
+    <body>
+<div class="main">
+  <h1 class="main-heading">
+    <span class="main-heading-primary">Credit</span>
+    <span class="main-heading-secondary">Management</span>
+  </h1>
+    </div>
+        <table>
+			<thead>
+				<tr>
+                    <th>S No.</th>
+    				<th>Name</th>
+    				<th>Email</th>
+    				<th>Credits</th>
+    				<th>Transfer</th>
+				</tr>
+			</thead>
+	
+
+            <!--fetch and display data from MySQL-->
+            <?php
+                $i=1;
+
+                while($row = mysqli_fetch_array($result)) {
+                    echo "<tr>";
+                    echo "<td>" . $i . "</td>";
+                    echo "<td>" . $row["name"] . "</td>";
+                    echo "<td>" . $row["email"] . "</td>";
+                    echo "<td>" . $row["credit"] . "</td>";
+                    echo "<td><a  href=transfer.php?name=" . $row['name'] . ">Select</a><td>";
+                    echo "</tr>";
+                    ++$i;
+                }
+            ?>
+
+        </table>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/js/bootstrap.min.js" integrity="sha384-ux8v3A6CPtOTqOzMKiuo3d/DomGaaClxFYdCu2HPMBEkf6x2xiDyJ7gkXU0MWwaD" crossorigin="anonymous"></script>
+    </body>
+</html>
